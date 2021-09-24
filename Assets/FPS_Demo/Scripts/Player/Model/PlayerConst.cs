2b7ec3b8f6a5567d002d3ae53c3716da8eaa8e2e
TuHLT <holethanhtu244@gmail.com>
2021-09-24 17:27:41 +0700

    - update map

--- Assets/FPS_Demo/Scripts/Player/Model/PlayerConst.cs
+++ Assets/FPS_Demo/Scripts/Player/Model/PlayerConst.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class PlayerConst
 {
-    public const float MOVEMENT_SPEED = 5;
+    public const float MOVEMENT_SPEED = 10;
     public const float ROTATION_SPEED = 50;
 }
