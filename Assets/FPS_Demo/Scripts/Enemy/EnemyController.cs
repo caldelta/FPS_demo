564f3b22c354c2ba1c38ca7218f67cfc0301b03b
TuHLT <holethanhtu244@gmail.com>
2021-09-25 14:04:39 +0700

    - add enemy can spawn

--- Assets/FPS_Demo/Scripts/Enemy/EnemyController.cs
+++ Assets/FPS_Demo/Scripts/Enemy/EnemyController.cs
@@ -13,9 +13,16 @@ public class EnemyController : SingletonMonoBehaviour<MonoBehaviour>
     // Start is called before the first frame update
     void Start()
     {
+
+        InvokeRepeating("Spawn", 0, 1);
         
     }
-
+    private void Spawn()
+    {
+        var zombie = m_enemyObject[Random.Range(0, 3)];
+        var spawnPoint = m_spawnPoints[Random.Range(0, 3)];
+        Instantiate(zombie, spawnPoint.position, zombie.transform.rotation, transform);
+    }
     // Update is called once per frame
     void Update()
     {
